f826ef3b360a1f6c73460d74cebb70c5bfa0e475
hankk <joseph10987@naver.com>
2023-03-15 12:51:29 +0900

    15day

--- ProcessPactice/src/trainReservation/entity/Seat.java
+++ ProcessPactice/src/trainReservation/entity/Seat.java
@@ -1,31 +1,37 @@
 package trainReservation.entity;
 
-//남아있는 좌석
 public class Seat {
 	private int roomNumber;
-	private String sratNumber;
+	private String seatNumber;
 	private boolean seatStatus;
 	
-	public Seat() { }
-	public Seat(int roomNumber, String sratNumber, boolean seatStatus) {
+	public Seat() {}
+
+	public Seat(int roomNumber, String seatNumber, boolean seatStatus) {
 		this.roomNumber = roomNumber;
-		this.sratNumber = sratNumber;
+		this.seatNumber = seatNumber;
 		this.seatStatus = seatStatus;
 	}
+
 	public int getRoomNumber() {
 		return roomNumber;
 	}
-	public String getSratNumber() {
-		return sratNumber;
+
+	public String getSeatNumber() {
+		return seatNumber;
 	}
+
 	public boolean isSeatStatus() {
 		return seatStatus;
 	}
+	
+	public void setSeatStatus(boolean seatStatus) {
+		this.seatStatus = seatStatus;
+	}
+
 	@Override
 	public String toString() {
-		return "Seat [roomNumber=" + roomNumber + ", sratNumber=" + sratNumber + ", seatStatus=" + seatStatus + "]";
+		return "Seat [roomNumber=" + roomNumber + ", seatNumber=" + seatNumber + ", seatStatus=" + seatStatus + "]";
 	}
-	
-	
 	
 }
