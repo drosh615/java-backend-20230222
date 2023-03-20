47a40777ce1c1113b0b812eacd6df508819bfdb6
hankk <joseph10987@naver.com>
2023-03-20 13:06:25 +0900

    18day

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/46/90b3c366c2c6001d1a6199d7aa128cc0
@@ -0,0 +1,6 @@
+package board.common.constant;
+
+public interface HttpStatus {
+	public static final String NOT_FOUND = "404 NOT FOUND";
+	public static final String BAD_REQUEST = "404 BAD REQUEST";
+}
--- ProcessPactice/src/board/common/constant/HttpStatus.java
+++ ProcessPactice/src/board/common/constant/HttpStatus.java
@@ -1,6 +1,7 @@
 package board.common.constant;
 
 public interface HttpStatus {
-	public static final String NOT_FOUND = "404 NOT FOUND";
-	public static final String BAD_REQUEST = "404 BAD REQUEST";
+	public static final String BAD_REQUEST = "400 Bad Request";
+	public static final String UNAUTHORIZED = "401 Unauthorized";
+	public static final String NOT_FOUND = "404 Not Found";
 }
